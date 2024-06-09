d1e7ec28b74b9208bcdda59ca47ad8f82d30dae8
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-06-09 16:12:11 +0300

    rofi_todo refactoring

--- rofi_todo/todo.sh
+++ rofi_todo/todo.sh
@@ -12,11 +12,11 @@ choice=$(echo "$options" | rofi -dmenu -i -p "Todo")
 [ -z "$choice" ] && exit 1
 
 case "$choice" in
-  "  Add todo item")
-    item=$(rofi -dmenu -i -p "Todo Item : " -theme ~/.config/rofi/oneliner.rasi)
-    [ -n "$item" ] && echo "$item" >>"$todolist_path"
-    ;;
-  *)
-    sed -i "/$choice/d" "$todolist_path"
-    ;;
+"  Add todo item")
+	item=$(rofi -dmenu -i -p "Todo Item : " -theme ~/.config/rofi/oneliner.rasi)
+	[ -n "$item" ] && echo "$item" >>"$todolist_path"
+	;;
+*)
+	sed -i "/$choice/d" "$todolist_path"
+	;;
 esac
