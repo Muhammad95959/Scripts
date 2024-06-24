c466ef04152a98cd3911e89812e789f683e90683
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-06-24 19:00:29 +0300

    deleted start_waydroid.sh

--- start_waydroid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "$(waydroid status | awk '/Session:/ {print $2}')" = "STOPPED" ]; then
-	cage -- waydroid show-full-ui
-else
-	sudo waydroid session stop && sudo waydroid container stop
-fi
