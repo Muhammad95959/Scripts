cc632f5794b7c819a22b8be15f27e723b4fd41e5
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-08-31 23:19:01 +0300

    updated move_to_i3_corners.sh

--- move_to_i3_corners.sh
+++ move_to_i3_corners.sh
@@ -20,27 +20,16 @@ window_height=$(echo "$window_info" | awk '/Height:/ {print $2}')
 position_x=10
 position_y=10
 
-while true; do
-  case $1 in
-  topleft)
-    break
-    ;;
-  topright)
-    position_x=$((screen_width - window_width - 10))
-    break
-    ;;
-  bottomleft)
-    position_y=$((screen_height - window_height - 10))
-    break
-    ;;
-  bottomright)
-    position_x=$((screen_width - window_width - 10))
-    position_y=$((screen_height - window_height - 10))
-    break
-    ;;
-  *) set -- "$(printf "(1) topleft\n(2) topright\n(3) bottomleft\n(4) bottomright" | rofi -dmenu -i -theme ~/.config/rofi/oneliner.rasi -p "corner:" | awk '{print $2}')" ;;
-  esac
-done
+[ -z "$1" ] && set -- "$(printf "(1) topleft\n(2) topright\n(3) bottomleft\n(4) bottomright" | rofi -dmenu -i -theme ~/.config/rofi/oneliner.rasi -p "corner:" | awk '{print $2}')"
 
-# Move the focused window to the calculated position
-i3-msg "[id=$window_id] floating enable, move position $position_x $position_y"
+case $1 in
+topleft) ;;
+topright) position_x=$((screen_width - window_width - 10)) ;;
+bottomleft) position_y=$((screen_height - window_height - 10)) ;;
+bottomright)
+  position_x=$((screen_width - window_width - 10))
+  position_y=$((screen_height - window_height - 10))
+  ;;
+esac
+
+[ -n "$1" ] && i3-msg "[id=$window_id] floating enable, move position $position_x $position_y"
