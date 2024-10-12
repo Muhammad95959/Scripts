f19f6cb6d3fde6b926c83de9cfb2611265e073b2
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-10-12 19:41:05 +0300

    add brave_history.sh + brave_bookmarks.sh

---
+++ brave_bookmarks.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Path to Brave bookmarks file
+BOOKMARKS_FILE="$HOME/.config/BraveSoftware/Brave-Browser/Default/Bookmarks"
+
+# Temporary files to hold names and URLs
+names_file=$(mktemp)
+urls_file=$(mktemp)
+
+# Get bookmark names and URLs using jq
+jq -r '
+    .roots.bookmark_bar.children[] |
+    if .children then
+        (.children[] | .name)
+    else
+        .name
+    end' "$BOOKMARKS_FILE" >"$names_file"
+
+jq -r '
+    .roots.bookmark_bar.children[] |
+    if .children then
+        (.children[] | .url)
+    else
+        .url
+    end' "$BOOKMARKS_FILE" >"$urls_file"
+
+# Show bookmark names in rofi and capture the selected line
+if pgrep waybar; then
+  selected_name=$(rofi -dmenu -i -p "Bookmark:" -theme "$HOME/.config/rofi/launcher.rasi" <"$names_file")
+else
+  selected_name=$(rofi -dmenu -i -p "Bookmark:" -theme "$HOME/.config/rofi/oneliner.rasi" <"$names_file")
+fi
+
+# Get the corresponding URL based on the selected name
+selected_url=$(awk -v name="$selected_name" '
+    FNR==NR { names[FNR]=$0; next }
+    { if (names[FNR] == name) print $0 }
+' "$names_file" "$urls_file")
+
+# If a URL was selected, open it in Brave
+if [ -n "$selected_url" ]; then
+  brave --test-type "$selected_url"
+fi
+
+# Clean up temporary files
+rm -f "$names_file" "$urls_file"
---
+++ brave_history.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if ! fuser ~/.config/BraveSoftware/Brave-Browser/Default/History >/dev/null 2>&1; then
+  cp ~/.config/BraveSoftware/Brave-Browser/Default/History /tmp/BraveHistory
+fi
+
+HISTORY_FILE="/tmp/BraveHistory"
+
+selected_entry=$(sqlite3 -separator "  󰛂  " "$HISTORY_FILE" "SELECT title, url FROM urls ORDER BY last_visit_time DESC;" | rofi -dmenu -i -p "History:")
+selected_url=$(echo "$selected_entry" | awk -F '  󰛂  ' '{print $2}')
+
+if [ -n "$selected_url" ]; then
+  export XDG_CONFIG_HOME=
+  brave --test-type "$selected_url"
+fi
