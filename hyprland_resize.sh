4ce8a166192ad29a9eaa7c25397a2984d81b4c93
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-10-16 12:03:17 +0300

    add hyprland_resize.sh

---
+++ hyprland_resize.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+options="\
+(a) 1800 x 825
+(s) 1600 x 900
+(d) 1280 x 720
+(f) 960 x 600
+(g) 840 x 525
+(h) 800 x 450
+(j) 640 x 360
+(k) 480 x 270
+(l) 432 x 243
+(;) 320 x 180"
+
+size=$(echo "$options" | rofi -dmenu -i -theme ~/.config/rofi/wayland/rofi/oneliner.rasi -p "choose the dimentions: " | awk '{print $2 " " $4}')
+test -z "$size" || hyprctl --batch "dispatch setfloating; dispatch resizeactive exact $size; dispatch centerwindow"
