30ae3a0bef6f334494881176a6199096372d7cd6
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-11-16 14:40:42 +0200

    update magicspace.sh

--- magicspace.sh
+++ magicspace.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 if hyprctl -j clients | jq '.[] | select(.workspace.name == "special:magic")' -r | grep -q .; then
-  echo "something"
   hyprctl dispatch togglespecialworkspace magic
 else
-  echo "nothing"
   hyprctl --batch "dispatch setfloating; dispatch centerwindow; dispatch movetoworkspace special:magic"
 fi
