7827782ef5b7fcf916c6d92f874cf8389e108539
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-11-17 16:17:42 +0200

    update magicspace.sh + backup_zsh_history.sh

--- backup_zsh_history.sh
+++ backup_zsh_history.sh
@@ -4,4 +4,5 @@ if [ ! -f /mnt/Disk_D/Muhammad/Linux_stuff/Backup/zsh_history/zsh_history_"$(dat
 	cp /home/muhammad/.zhistory /mnt/Disk_D/Muhammad/Linux_stuff/Backup/zsh_history/zsh_history_"$(date +%Y-%m-%d)".bak
   sudo timeshift --create
   ~/Scripts/update_DotFiles_repo.sh
+  sudo updatedb &
 fi
--- magicspace.sh
+++ magicspace.sh
@@ -3,5 +3,5 @@
 if hyprctl -j clients | jq '.[] | select(.workspace.name == "special:magic")' -r | grep -q .; then
   hyprctl dispatch togglespecialworkspace magic
 else
-  hyprctl --batch "dispatch setfloating; dispatch centerwindow; dispatch movetoworkspace special:magic"
+  hyprctl --batch "dispatch setfloating; dispatch centerwindow; dispatch movetoworkspace special:magic; dispatch togglespecialworkspace magic"
 fi
