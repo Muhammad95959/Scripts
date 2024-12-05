0a22364ba63200a5aba5d20d45ef78a636dff1a7
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-12-05 16:19:36 +0200

    use brightnessctl instead of brillo in brightness_changer.sh

--- brightness_changer.sh
+++ brightness_changer.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-OLD_BRIT=$(brillo)
+OLD_BRIT=$(brightnessctl info | awk -F '[%(]' '/%/ {print $2}')
 
 case $1 in
-inc) brillo -A 5 ;;
-dec) brillo -U 5 ;;
-exp-inc) brillo -A 0.5 ;;
-exp-dec) brillo -U 0.5 ;;
+inc) brightnessctl set +5% ;;
+dec) brightnessctl set 5%- ;;
+exp-inc) brightnessctl set +1% ;;
+exp-dec) brightnessctl set 1%- ;;
 esac
 
-BRIT=$(brillo)
+BRIT=$(brightnessctl info | awk -F '[%(]' '/%/ {print $2}')
 if [ "$BRIT" != "$OLD_BRIT" ]; then
 	volnoti-show "$BRIT" -s /usr/share/pixmaps/volnoti/display-brightness-symbolic.svg
 	OLD_BRIT=$BRIT
