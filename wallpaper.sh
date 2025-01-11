08e12c90df4c52ecfe83a0a33bdf6b15342fe07d
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-01-11 23:08:35 +0200

    update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 wallpapers_paths="/mnt/Disk_D/Backgrounds"
-current_wallpaper_link="$HOME/.cache/${XDG_SESSION_TYPE}wall"
-current_wallpaper=$(readlink "$current_wallpaper_link")
 wallpapers_list=$(eval fd .jpg "$wallpapers_paths" | sed -E 's/ /\\r/g')
+[ -f "$HOME/.cache/${XDG_SESSION_TYPE}wall" ] || ln -fs "$(echo "$wallpapers_list" | sed -n "1p")" "$HOME/.cache/${XDG_SESSION_TYPE}wall"
+current_wallpaper=$(readlink "$HOME/.cache/${XDG_SESSION_TYPE}wall")
 current_wallpaper_index=$(echo "$wallpapers_list" | grep -n "$current_wallpaper" | awk -F ':' '{print $1}')
 selected_index=$(
   for a in $wallpapers_list; do
@@ -16,9 +16,9 @@ selected_wallpaper_path=$(echo "$wallpapers_list" | sed -n "$((selected_index + 
 
 if [ "$XDG_SESSION_TYPE" = "x11" ]; then
   feh --no-fehbg --bg-fill "$selected_wallpaper_path"
-  ln -fs "$selected_wallpaper_path" ~/.cache/x11wall
 elif [ "$XDG_SESSION_TYPE" = "wayland" ]; then
   pgrep -x swww-daemon >/dev/null 2>&1 || setsid swww-daemon >/dev/null 2>&1 &
   swww img "$selected_wallpaper_path" --transition-type "fade" --transition-duration 1
-  ln -fs "$selected_wallpaper_path" ~/.cache/waylandwall
 fi
+
+ln -fs "$selected_wallpaper_path" "$HOME/.cache/${XDG_SESSION_TYPE}wall"
