dd2e3429b5aa2e18d7090f915964bf7193399ad5
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-01-11 06:53:02 +0200

    update wallpaper.sh + myWebsites.sh + mpv_history.sh

--- mpv_history.sh
+++ mpv_history.sh
@@ -3,6 +3,6 @@
 filtered=$(tac "$HOME"/.config/mpv/history.log | sed 's/^.*| //' | while read -r line; do
   [ -e "$line" ] && echo "$line"
 done)
-selected=$(echo "$filtered" | rofi -dmenu -p "Select Video:")
+selected=$(echo "$filtered" | rofi -dmenu -i -p "Select Video:")
 [ -z "$selected" ] && exit
 mpv "$selected" >/dev/null 2>&1 &
--- wallpaper.sh
+++ wallpaper.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-wallpapers_path="/mnt/Disk_D/Backgrounds"
-current_wallpaper="$HOME/.cache/${XDG_SESSION_TYPE}wall"
-selected=$(
-  for a in $(fd .jpg $wallpapers_path --max-depth 1); do
-    echo -en "$(basename "$a")\0icon\x1f$a\n"
-  done | rofi -dmenu -theme ~/.config/rofi/wallpaper.rasi -theme-str "inputbar {background-image: url(\"$current_wallpaper\", width);}"
+wallpapers_paths="/mnt/Disk_D/Backgrounds"
+current_wallpaper_link="$HOME/.cache/${XDG_SESSION_TYPE}wall"
+current_wallpaper=$(readlink "$current_wallpaper_link")
+wallpapers_list=$(eval fd .jpg "$wallpapers_paths" | sed -E 's/ /\\r/g')
+current_wallpaper_index=$(echo "$wallpapers_list" | grep -n "$current_wallpaper" | awk -F ':' '{print $1}')
+selected_index=$(
+  for a in $wallpapers_list; do
+    printf '%s\0icon\x1f%s\n' "$(basename "$a")" "$a"
+  done | rofi -dmenu -format i -theme ~/.config/rofi/wallpaper.rasi -theme-str "inputbar {background-image: url(\"$current_wallpaper\", width);}" -selected-row $((current_wallpaper_index - 1))
 )
+selected_wallpaper_path=$(echo "$wallpapers_list" | sed -n "$((selected_index + 1))p")
 
-[ -z "$selected" ] && exit
+[ -z "$selected_index" ] && exit
 
-if [ "$XDG_SESSION_TYPE" = "x11" ];then
-  feh --bg-fill "$wallpapers_path/$selected"
-  ln -fs "$wallpapers_path/$selected" ~/.cache/x11wall
-elif [ "$XDG_SESSION_TYPE" = "wayland" ];then
+if [ "$XDG_SESSION_TYPE" = "x11" ]; then
+  feh --no-fehbg --bg-fill "$selected_wallpaper_path"
+  ln -fs "$selected_wallpaper_path" ~/.cache/x11wall
+elif [ "$XDG_SESSION_TYPE" = "wayland" ]; then
   pgrep -x swww-daemon >/dev/null 2>&1 || setsid swww-daemon >/dev/null 2>&1 &
-  swww img "$wallpapers_path/$selected" --transition-type "fade" --transition-duration 1
-  ln -fs "$wallpapers_path/$selected" ~/.cache/waylandwall
+  swww img "$selected_wallpaper_path" --transition-type "fade" --transition-duration 1
+  ln -fs "$selected_wallpaper_path" ~/.cache/waylandwall
 fi
