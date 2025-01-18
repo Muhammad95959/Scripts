7c3fe323d0989f2b8f4e1c890b60c3d54b36d588
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-01-19 00:12:38 +0200

    fix bilal.sh 60 minutes bug

--- bilal.sh
+++ bilal.sh
@@ -1,31 +1,39 @@
 #!/bin/sh
 
 if [ "$1" = "-a" ]; then
-	bilal all | sed -E 's/Sherook/Shuruk/; s/Dohr/Dhuhr/; s/Ma?ghreb/Maghrib/'
+  bilal all | sed -E 's/Sherook/Shuruk/; s/Dohr/Dhuhr/; s/Ma?ghreb/Maghrib/'
 elif [ "$1" = "-r" ]; then
-	next_salah=$(bilal next | awk '{print $1}')
-	time_value=$(bilal current | awk -F '(' '{print $2}' | awk 'BEGIN{OFS=" ";} {print $1}')
-	time_scale=$(bilal current | awk -F '(' '{print $2}' | awk 'BEGIN{OFS=" ";} {print $2}')
-	if [ "$time_scale" = "hours" ]; then
-		hours=$(printf "%s" "$time_value" | awk -F ':' '{print $1}')
-		minutes=$(printf "%s" "$time_value" | awk -F ':' '{print $2}')
-		[ "$(printf "%s" "$hours" | wc -c)" -lt 2 ] && hours="0$hours"
-		[ "$(printf "%s" "$minutes" | wc -c)" -lt 2 ] && minutes="0$minutes"
-		time_value="$hours:$minutes"
-	elif [ "$time_scale" = "minutes" ]; then
-		[ "$(printf "%s" "$time_value" | wc -c)" -lt 2 ] && time_value="0$time_value"
-		time_value="00:$time_value"
-	fi
-	current_time_in_minutes="$(($(date +"%H" | sed -E 's/^0?//') * 60 + $(date +"%M" | sed -E 's/^0?//')))"
-	previous_salah_name=$(bilal current | awk '{print $1}' | sed 's/Maghreb/Mghreb/')
+  next_salah=$(bilal next | awk '{print $1}')
+  time_value=$(bilal current | awk -F '(' '{print $2}' | awk 'BEGIN{OFS=" ";} {print $1}')
+  time_scale=$(bilal current | awk -F '(' '{print $2}' | awk 'BEGIN{OFS=" ";} {print $2}')
+  if [ "$time_scale" = "hours" ]; then
+    hours=$(printf "%s" "$time_value" | awk -F ':' '{print $1}')
+    minutes=$(printf "%s" "$time_value" | awk -F ':' '{print $2}')
+    if [ "$minutes" = "60" ]; then
+      minutes="00"
+      hours=$((hours + 1))
+    fi
+    [ "$(printf "%s" "$hours" | wc -c)" -lt 2 ] && hours="0$hours"
+    [ "$(printf "%s" "$minutes" | wc -c)" -lt 2 ] && minutes="0$minutes"
+    time_value="$hours:$minutes"
+  elif [ "$time_scale" = "minutes" ]; then
+    if [ "$time_value" = "60" ]; then
+      time_value="01:00"
+    else
+      [ "$(printf "%s" "$time_value" | wc -c)" -lt 2 ] && time_value="0$time_value"
+      time_value="00:$time_value"
+    fi
+  fi
+  current_time_in_minutes="$(($(date +"%H" | sed -E 's/^0?//') * 60 + $(date +"%M" | sed -E 's/^0?//')))"
+  previous_salah_name=$(bilal current | awk '{print $1}' | sed 's/Maghreb/Mghreb/')
   previous_salah_time=$(bilal all | awk -v p="$previous_salah_name" -F ': ' '{if (p == "Jumua") p = "Dohr"} $1 == p {print $2}')
-	previous_salah_time_in_minutes="$((\
-		$(echo "$previous_salah_time" | sed -E 's/:.*$//; s/^0?//') * 60 + \
-		$(echo "$previous_salah_time" | awk -F ':' '{print $2}' | sed -E 's/^0?//')))"
-	time_passed_since_previous_salah="$((current_time_in_minutes - previous_salah_time_in_minutes))"
-	if [ "$time_passed_since_previous_salah" -le 30 ] && [ "$time_passed_since_previous_salah" -ge 0 ]; then
-		printf "%d min after %s" "$time_passed_since_previous_salah" "$previous_salah_name" | sed -E 's/Sherook/Shuruk/; s/Dohr/Dhuhr/; s/Ma?ghreb/Maghrib/'
-	else
-		printf "%s until %s\n" "$time_value" "$next_salah" | sed -E 's/Sherook/Shuruk/; s/Dohr/Dhuhr/; s/Ma?ghreb/Maghrib/'
-	fi
+  previous_salah_time_in_minutes="$((\
+    $(echo "$previous_salah_time" | sed -E 's/:.*$//; s/^0?//') * 60 + \
+    $(echo "$previous_salah_time" | awk -F ':' '{print $2}' | sed -E 's/^0?//')))"
+  time_passed_since_previous_salah="$((current_time_in_minutes - previous_salah_time_in_minutes))"
+  if [ "$time_passed_since_previous_salah" -le 30 ] && [ "$time_passed_since_previous_salah" -ge 0 ]; then
+    printf "%d min after %s" "$time_passed_since_previous_salah" "$previous_salah_name" | sed -E 's/Sherook/Shuruk/; s/Dohr/Dhuhr/; s/Ma?ghreb/Maghrib/'
+  else
+    printf "%s until %s\n" "$time_value" "$next_salah" | sed -E 's/Sherook/Shuruk/; s/Dohr/Dhuhr/; s/Ma?ghreb/Maghrib/'
+  fi
 fi
