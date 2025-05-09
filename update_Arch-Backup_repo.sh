1f631f4e1b49b0228e3ece80c2f1c2df09b820cd
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-05-10 00:29:39 +0300

    update update_DotFiles_repo.sh and rename it to update_Arch-Backup_repo.sh

---
+++ update_Arch-Backup_repo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp  -r /usr/share/pixmaps/volnoti                    /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
+cp  /usr/lib/systemd/system/kanata.service           /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
+cp  /etc/NetworkManager/conf.d/20-connectivity.conf  /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
+cp  /etc/environment                                 /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
+cp  /etc/modprobe.d/nobeep.conf                      /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
+cp  /etc/samba/smb.conf                              /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
+
+notify-send -t 3000 "updated Arch-Backup repo"
