86abc6e1806febd623029a630c05c4a453216857
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-05-28 16:39:51 +0300

    add a command to make backups for greenclip.history

--- backup_zsh_history.sh
+++ backup_zsh_history.sh
@@ -2,6 +2,7 @@
 
 if [ ! -f /mnt/Disk_D/Muhammad/Linux_stuff/Backup/zsh_history/zsh_history_"$(date +%Y-%m-%d)".bak ]; then
 	cp /home/muhammad/.zhistory /mnt/Disk_D/Muhammad/Linux_stuff/Backup/zsh_history/zsh_history_"$(date +%Y-%m-%d)".bak
+	cp /home/muhammad/.cache/greenclip.history /mnt/Disk_D/Muhammad/Linux_stuff/Backup/greenclip_history/greenclip_history_"$(date +%Y-%m-%d)".bak
   sudo timeshift --create
   ~/Scripts/update_DotFiles_repo.sh
   sudo updatedb &
