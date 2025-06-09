f0b0354c321ac230595996d442b1d8bbe297466d
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-06-09 14:51:30 +0300

    update backup_zsh_history.sh

--- backup_zsh_history.sh
+++ backup_zsh_history.sh
@@ -4,6 +4,6 @@ if [ ! -f /mnt/Disk_D/Muhammad/Linux_stuff/Backup/zsh_history/zsh_history_"$(dat
 	cp /home/muhammad/.zhistory /mnt/Disk_D/Muhammad/Linux_stuff/Backup/zsh_history/zsh_history_"$(date +%Y-%m-%d)".bak
 	cp /home/muhammad/.cache/greenclip.history /mnt/Disk_D/Muhammad/Linux_stuff/Backup/greenclip_history/greenclip_history_"$(date +%Y-%m-%d)".bak
   sudo timeshift --create
-  ~/Scripts/update_DotFiles_repo.sh
+  ~/Scripts/update_Arch-Backup_repo.sh
   sudo updatedb &
 fi
