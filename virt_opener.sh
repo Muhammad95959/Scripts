00922f36764e9e9269b31c7a36d7a2a807034808
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-06-15 21:33:47 +0300

    update virt_opener.sh

--- virt_opener.sh
+++ virt_opener.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 wlrctl pointer move -1920 -1080 || xdotool mousemove 0 0
+sudo systemctl start libvirtd
 virsh -c qemu:///system start "$1" && virt-manager --connect qemu:///system --show-domain-console "$1" || virsh -c qemu:///system destroy "$1"
