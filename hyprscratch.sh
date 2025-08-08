a1d73ca5e746cc1e8f20883cb52ca3f3b79fdfdf
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-08-08 14:30:57 +0300

    update hyprscratch.sh

--- hyprscratch.sh
+++ hyprscratch.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
 
-scratchWindowsCount=$(hyprctl workspaces -j | jq '.[] | select(.name == "special:scratchpad") | .windows')
-isFloating=$(hyprctl activewindow -j | jq '.floating')
+currentAddress=$(hyprctl activewindow -j | jq -r '.address')
+[ -f /tmp/scratchpad ] && address=$(cat /tmp/scratchpad)
 
-if [ -z "$scratchWindowsCount" ]; then
-  [ "$isFloating" = "false" ] && hyprctl --batch "dispatch setfloating; dispatch resizeactive exact 1280 720; dispatch centerwindow"
-  scratchpad
+if [ "$1" = "-t" ]; then
+  if [ "$currentAddress" = "$address" ]; then
+    rm -f /tmp/scratchpad
+  fi
   exit
 fi
 
-scratchpad -g -m "rofi -dmenu -p 'scratchpad: ' -theme ~/.config/rofi/script_chooser.rasi"
+scratchWindowsCount=$(hyprctl workspaces -j | jq 'map(select(.name == "special:scratchpad"))[0].windows // 0')
+isFloating=$(hyprctl activewindow -j | jq -r '.floating')
+
+if [ -n "$address" ]; then
+  windowExists=$(hyprctl clients -j | jq -e --arg addr "$address" 'map(select(.address == $addr)) | length > 0')
+  if [ "$windowExists" = "true" ]; then
+    echo "scratchpad already exists."
+    [ "$currentAddress" = "$address" ] && scratchpad && exit
+    workspace=$(hyprctl activeworkspace -j | jq -r '.id')
+    hyprctl dispatch movetoworkspacesilent "$workspace,address:$address"
+    hyprctl dispatch focuswindow address:"$address"
+    exit
+  fi
+fi
+
+if [ "$scratchWindowsCount" = "0" ]; then
+  [ "$isFloating" = "false" ] && hyprctl --batch "dispatch setfloating; dispatch resizeactive exact 1600 900; dispatch centerwindow"
+  hyprctl activewindow -j | jq -r '.address' >/tmp/scratchpad
+  scratchpad
+else
+  scratchpad -g -m "rofi -dmenu -p 'scratchpad: ' -theme ~/.config/rofi/script_chooser.rasi"
+fi
