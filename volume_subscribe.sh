0693f142c5e971bf4d8e5b6d2abd7e50db4cf84d
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-08-15 02:44:34 +0300

    update volume_subscribe.sh

--- volume_subscribe.sh
+++ volume_subscribe.sh
@@ -3,14 +3,16 @@
 MAX_VOL=150
 OLD_VOL=$(($(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100}' | sed 's/\.//') * 100 / MAX_VOL))
 pw-mon | grep --line-buffered "sink" |
-while read -r _; do
-	VOL=$(($(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100}' | sed 's/\.//') * 100 / MAX_VOL))
+  while read -r _; do
+    UNFORMATTED_VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100}' | sed 's/\.//')
+    VOL=$((UNFORMATTED_VOL * 100 / MAX_VOL))
+    [ "$UNFORMATTED_VOL" -eq 153 ] && wpctl set-volume @DEFAULT_AUDIO_SINK@ 1.5 && continue
     if [ $VOL != $OLD_VOL ] && [ $VOL != 0 ]; then
       echo $VOL
-		volnoti-show $VOL
-		OLD_VOL=$VOL
+      volnoti-show $VOL
+      OLD_VOL=$VOL
     elif [ $VOL != $OLD_VOL ] && [ $VOL = 0 ]; then
-        volnoti-show -m
-		OLD_VOL=$VOL
-	fi
-done
+      volnoti-show -m
+      OLD_VOL=$VOL
+    fi
+  done
