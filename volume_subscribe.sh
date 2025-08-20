0644f34a2a24826443fdce48d7405384a0f3b559
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-08-20 16:42:16 +0300

    remove useless sed from volume_subscribe.sh

--- volume_subscribe.sh
+++ volume_subscribe.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 MAX_VOL=150
-OLD_VOL=$(($(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100}' | sed 's/\.//') * 100 / MAX_VOL))
+OLD_VOL=$(($(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100}') * 100 / MAX_VOL))
 pw-mon | grep --line-buffered "sink" |
   while read -r _; do
-    UNFORMATTED_VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100}' | sed 's/\.//')
+    UNFORMATTED_VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100}')
     VOL=$((UNFORMATTED_VOL * 100 / MAX_VOL))
     [ "$UNFORMATTED_VOL" -eq 153 ] && wpctl set-volume @DEFAULT_AUDIO_SINK@ 1.5 && continue
     if [ $VOL != $OLD_VOL ] && [ $VOL != 0 ]; then
