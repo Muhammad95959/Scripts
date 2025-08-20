75537960744ee5441cb690e1323a2bd3ace37895
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-08-20 16:41:30 +0300

    add wayland theme for rofi in mpv_history.sh

--- mpv_history.sh
+++ mpv_history.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    rofi_theme_arg="-theme"
+    rofi_theme_path="$HOME/.config/rofi/wayland/rofi/config.rasi"
+else
+    rofi_theme_arg=""
+    rofi_theme_path=""
+fi
+
 filtered=$(tac "$HOME"/.config/mpv/history.log | sed 's/^.*| //' | while read -r line; do
   [ -e "$line" ] && echo "$line"
 done)
-selected=$(echo "$filtered" | rofi -dmenu -i -p "Select Video:")
+selected=$(echo "$filtered" | rofi -dmenu -i -p "Select Video:" "$rofi_theme_arg" "$rofi_theme_path")
 [ -z "$selected" ] && exit
 mpv "$selected" >/dev/null 2>&1 &
