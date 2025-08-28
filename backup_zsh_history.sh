2c959e04343b57af3e9de374fdb763fbb9b7112f
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-08-28 14:49:55 +0300

    add backup_brave_history.sh

--- backup_zsh_history.sh
+++ backup_zsh_history.sh
@@ -2,8 +2,9 @@
 
 if [ ! -f /mnt/Disk_D/Muhammad/Linux_stuff/Backup/zsh_history/zsh_history_"$(date +%Y-%m-%d)".bak ]; then
 	cp /home/muhammad/.zhistory /mnt/Disk_D/Muhammad/Linux_stuff/Backup/zsh_history/zsh_history_"$(date +%Y-%m-%d)".bak
-	cp /home/muhammad/.cache/greenclip.history /mnt/Disk_D/Muhammad/Linux_stuff/Backup/greenclip_history/greenclip_history_"$(date +%Y-%m-%d)".bak
+	cp /home/muhammad/.local/share/greenclip.history /mnt/Disk_D/Muhammad/Linux_stuff/Backup/greenclip_history/greenclip_history_"$(date +%Y-%m-%d)".bak
   sudo timeshift --create
+  ~/Scripts/backup_brave_history.sh
   ~/Scripts/update_Arch-Backup_repo.sh
   sudo updatedb &
 fi
--- brave_history.sh
+++ brave_history.sh
@@ -5,10 +5,20 @@ if ! fuser ~/.config/BraveSoftware/Brave-Browser/Default/History >/dev/null 2>&1
 fi
 
 HISTORY_FILE="/tmp/BraveHistory"
+selected_entry=$(
+  {
+    echo "󰘳  Open Backup History"
+    sqlite3 -separator "  󰛂  " "$HISTORY_FILE" \
+      "SELECT title, url FROM urls ORDER BY last_visit_time DESC;"
+  } | rofi -dmenu -no-custom -i -p "History:"
+)
 
-selected_entry=$(sqlite3 -separator "  󰛂  " "$HISTORY_FILE" "SELECT title, url FROM urls ORDER BY last_visit_time DESC;" | rofi -dmenu -no-custom -i -p "History:")
+if [ "$selected_entry" = "󰘳  Open Backup History" ]; then
+  HISTORY_FILE="/mnt/Disk_D/Muhammad/Linux_stuff/Backup/BraveHistoryArchive.db"
+  selected_entry=$(sqlite3 -separator "  󰛂  " "$HISTORY_FILE" "SELECT title, url FROM urls ORDER BY last_visit_time DESC;" | rofi -dmenu -no-custom -i -p "Backup History:")
+fi
+
 selected_url=$(echo "$selected_entry" | awk -F '  󰛂  ' '{print $2}')
-
 if [ -n "$selected_url" ]; then
   export XDG_CONFIG_HOME=
   brave --test-type "$selected_url"
