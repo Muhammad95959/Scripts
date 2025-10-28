8cd4f47d59eb9c5c456a77b485125171f3af573f
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-10-28 14:33:15 +0300

    add move_to_hyprland_corners.sh

---
+++ move_to_hyprland_corners.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Get the screen dimensions
+screen_width=1920
+screen_height=1080
+
+[ -z "$1" ] && set -- "$(printf "(1) topleft\n(2) topright\n(3) bottomleft\n(4) bottomright" | rofi -dmenu -no-custom -i -theme ~/.config/rofi/wayland/rofi/oneliner.rasi -p "corner:" | awk '{print $2}')"
+
+# Get the window ID of the currently focused window
+window_address=$(hyprctl activewindow -j | jq '.address' | sed 's/"//g')
+
+# Make the window floating
+[ -n "$1" ] && hyprctl dispatch setfloating address:"$window_address" && hyprctl dispatch centerwinon address:"$window_address"
+
+# Calculate the position
+position_x=10
+position_y=30
+
+# Get the window dimensions
+window_width=$(hyprctl clients -j | jq --arg addr "$window_address" '.[] | select(.address == $addr) | .size[0]')
+window_height=$(hyprctl clients -j | jq --arg addr "$window_address" '.[] | select(.address == $addr) | .size[1]')
+
+case $1 in
+topleft) ;;
+topright) position_x=$((screen_width - window_width - 10)) ;;
+bottomleft) position_y=$((screen_height - window_height - 10)) ;;
+bottomright)
+  position_x=$((screen_width - window_width - 10))
+  position_y=$((screen_height - window_height - 10))
+  ;;
+esac
+
+[ -n "$1" ] && hyprctl dispatch movewindowpixel exact "$position_x" "$position_y",address:"$window_address"
