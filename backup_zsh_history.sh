259a33837663477a395ebda236d5ae96fbb850c0
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-10-28 17:18:07 +0300

    remove backup_brave_history

--- backup_zsh_history.sh
+++ backup_zsh_history.sh
@@ -4,7 +4,6 @@ if [ ! -f /mnt/Disk_D/Muhammad/Linux-Backup/zsh_history/zsh_history_"$(date +%Y-
 	cp /home/muhammad/.zhistory /mnt/Disk_D/Muhammad/Linux-Backup/zsh_history/zsh_history_"$(date +%Y-%m-%d)".bak
 	cp /home/muhammad/.local/share/greenclip.history /mnt/Disk_D/Muhammad/Linux-Backup/greenclip_history/greenclip_history_"$(date +%Y-%m-%d)".bak
   sudo timeshift --create
-  ~/Scripts/backup_brave_history.sh
   ~/Scripts/update_Arch-Backup_repo.sh
   sudo updatedb &
 fi
