ee5b21198a6b1a5b933ff96033b60814134757fc
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2026-04-09 13:13:41 +0200

    update update_Arch-Backup_repo.sh

--- update_Arch-Backup_repo.sh
+++ update_Arch-Backup_repo.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
 
-cp  -r /usr/share/pixmaps/volnoti                              /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
-cp  /etc/NetworkManager/conf.d/20-connectivity.conf            /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
-cp  /usr/share/albert/widgetsboxmodel-ng/themes/Arc\ Dark.ini  /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
-cp  /etc/environment                                           /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
-cp  /etc/modprobe.d/nobeep.conf                                /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
-cp  /etc/samba/smb.conf                                        /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
-cp  /usr/lib/systemd/system/kanata.service                     /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/
+set -eu
 
-notify-send -t 3000 "updated Arch-Backup repo"
+DEST="/mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files"
+
+bak() {
+  src="$1"
+  if [ -e "$src" ]; then
+    cp "$src" "$DEST/"
+    echo "  ✓ $src"
+  else
+    echo "  ✗ missing (skipped): $src" >&2
+  fi
+}
+
+bak /etc/NetworkManager/conf.d/20-connectivity.conf
+bak /etc/environment
+bak /etc/modprobe.d/nobeep.conf
+bak /etc/samba/smb.conf
+bak /usr/lib/sddm/sddm.conf.d/default.conf
+bak /etc/systemd/system/kanata.service
+bak /usr/local/bin/bilal
+bak /usr/local/bin/confetti
+bak /usr/local/bin/hyprland-minimizer
+bak /usr/share/albert/widgetsboxmodel/themes/Arc\ Dark.ini
+bak /usr/share/albert/widgetsboxmodel/themes/Tokyonight\ Dark.ini
+bak /usr/share/sddm/themes/simple-sddm/theme.conf
+
+notify-send -t 3000 "Arch-Backup updated"
